2e691e1822dcd8e6a3d2a6e18e53de4875f463ea
Abinavan <inkaranabinavan2004@gmail.com>
2026-08-23 08:47:43 +0530

    initial varsion uploaded

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 
         public static void main(String[] args) throws Exception {
-            // If no arguments provided in IntelliJ, use defaults:
+
             String csvFilePath = (args.length > 0) ? args[0] : "sales.csv";
             String outputMethod = (args.length > 1) ? args[1] : "console";
             String outputFilePath = (args.length > 2) ? args[2] : "report.txt";
